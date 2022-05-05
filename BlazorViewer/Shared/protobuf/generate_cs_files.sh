a904c7acaba0c27c4e417538ef044e2b157b8f3e
Daniil <chuprikovdaniil0@gmail.com>
2022-05-05 14:42:16 +0700

    add batch script

--- BlazorViewer/Shared/protobuf/generate_cs_files.sh
+++
@@ -1,9 +0,0 @@
-full_path=$(realpath $0)
-
-dir_path=$(dirname $full_path)
-
-mkdir -p $dir_path/DesignModel
-
-protoc -I=$dir_path/design --csharp_out=$dir_path/DesignModel --csharp_opt=file_extension=.g.cs,base_namespace=DesignModel $dir_path/design/*.proto $dir_path/design/math/*.proto $dir_path/design/appearance/*.proto $dir_path/design/content/controls/*.proto $dir_path/design/content/*.proto
-
-read
