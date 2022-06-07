d3c90c71a4c17296da930bd3f298f7c32ec88dfc
Ержан Женатов <erzhan.97531@gmail.com>
2022-06-07 23:34:19 +0700

    Add visualization of the selection

--- BlazorViewer/BlazorExtensions/Renderer.cs
+++ BlazorViewer/BlazorExtensions/Renderer.cs
@@ -28,9 +28,28 @@ namespace BlazorExtensions
                 foreach(Element element in layer.Elements)
                 {
                     Console.WriteLine("Element");
-                    ModelFactory.Build(element).Draw(context);
+                    await ModelFactory.Build(element).Draw(context);
                 }
             }
+        }
+        public async Task RenderSelection(Element element)
+        {
+            Console.WriteLine("Render");
+
+            var x = element.Position.X + element.Content.ClosedVector.Controls.Rectangle.Corner1.X;
+            var y = element.Position.Y + element.Content.ClosedVector.Controls.Rectangle.Corner1.Y;
+            var width = element.Content.ClosedVector.Controls.Rectangle.Corner2.X - element.Content.ClosedVector.Controls.Rectangle.Corner1.X;
+            var height = element.Content.ClosedVector.Controls.Rectangle.Corner2.Y - element.Content.ClosedVector.Controls.Rectangle.Corner1.Y;
+
+            var r = element.Content.ClosedVector.Fill.Solid.Color.Process.Rgb.R;
+            var g = element.Content.ClosedVector.Fill.Solid.Color.Process.Rgb.G;
+            var b = element.Content.ClosedVector.Fill.Solid.Color.Process.Rgb.B;
+
+            await context.SetFillStyleAsync($"rgb({r},{g},{b})");
+            await context.FillRectAsync(x, y, width, height);
+
+            await context.SetStrokeStyleAsync("yellow");
+            await context.StrokeRectAsync(x,y,width,height);  
         }
     }
 }
--- BlazorViewer/BlazorExtensions/Strategies/RectangleDrawStrategy.cs
+++ BlazorViewer/BlazorExtensions/Strategies/RectangleDrawStrategy.cs
@@ -11,8 +11,8 @@ namespace BlazorExtensions.Strategies
 
         public override async Task Draw(Canvas2DContext context)
         {
-            var x = element.Content.ClosedVector.Controls.Rectangle.Corner1.X;
-            var y = element.Content.ClosedVector.Controls.Rectangle.Corner1.Y;
+            var x = element.Position.X+element.Content.ClosedVector.Controls.Rectangle.Corner1.X;
+            var y = element.Position.Y+element.Content.ClosedVector.Controls.Rectangle.Corner1.Y;
             var width = element.Content.ClosedVector.Controls.Rectangle.Corner2.X - element.Content.ClosedVector.Controls.Rectangle.Corner1.X;
             var height = element.Content.ClosedVector.Controls.Rectangle.Corner2.Y - element.Content.ClosedVector.Controls.Rectangle.Corner1.Y;
             var r = element.Content.ClosedVector.Fill.Solid.Color.Process.Rgb.R;
